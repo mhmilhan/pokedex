34103cfa97409819546c6b2d30e601fe562a1ff9
Hasan Milhan <mahmudulhasanmilhan@gmail.com>
2025-11-20 23:04:51 +0600

    using playwright, solved it, exercise 11.9 done

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,19 +1,20 @@
-const { test, describe, expect } = require("@playwright/test");
+/* eslint-env node */
+const { test, describe, expect } = require('@playwright/test')
 
-describe("Pokedex", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("");
-    await expect(page.getByText("ivysaur")).toBeVisible();
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
-        "Pokémon and Pokémon character names are trademarks of Nintendo."
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
-    ).toBeVisible();
-  });
+    ).toBeVisible()
+  })
 
-  test("pokemon page can be navigated to", async ({ page }) => {
-    await page.goto("");
-    await page.click("text=ivysaur");
-    await expect(page.getByText("chlorophyll")).toBeVisible();
-  });
-});
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.click('text=ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
